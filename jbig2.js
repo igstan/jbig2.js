311d69d45bce5fc00945e8199643148b248d39cb
Ionuț G. Stan <ionut.g.stan@gmail.com>
2012-04-09 20:12:40 +0300

    Turn on strict mode in jbig2.js

--- jbig2.js
+++ jbig2.js
@@ -1,4 +1,6 @@
 ;(function (global) {
+  "use strict";
+
   var SEQUENTIAL    = "SEQUENTIAL";
   var RANDOM_ACCESS = "RANDOM_ACCESS";
   var controlHeader = [0x97, 0x4A, 0x42, 0x32, 0x0D, 0x0A, 0x1A, 0x0A];
