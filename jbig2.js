5c46747f04b9c394ea3223b745a327a0e23a7c0f
Ionuț G. Stan <ionut.g.stan@gmail.com>
2012-04-09 19:51:14 +0300

    Move functionality developed so far from play.html to jbig2.js

--- jbig2.js
+++ jbig2.js
@@ -1,10 +1,44 @@
 ;(function (global) {
 
+  var controlHeader = [0x97, 0x4A, 0x42, 0x32, 0x0D, 0x0A, 0x1A, 0x0A];
+
+  var int32 = function (number) {
+    return (number[0] << 24) | (number[1] << 16) | (number[2] << 8) | number[3];
+  };
+
+  var parseHeaderFlags = function (octet, fileHeaderFlags) {
+    if ((octet & 0xfc) != 0) {
+      throw new Error("Reserved bits (2-7) of file header flags are not zero");
+    }
+
+    fileHeaderFlags.sequential         = (octet & 1) === 1;
+    fileHeaderFlags.unknownPageNumbers = (octet & 2) === 1;
+
+    return fileHeaderFlags;
+  };
+
+  var decodeHeader = function (buffer) {
+    controlHeader.forEach(function (n, i) {
+      if (buffer[i] !== n) {
+        throw new Error("Control header check has failed");
+      }
+    });
+
+    var fileHeaderFlags = {};
+    var headerFlags = buffer[8];
+    fileHeaderFlags = parseHeaderFlags(headerFlags, fileHeaderFlags);
+
+    if (!fileHeaderFlags.unknownPageNumbers) {
+      console.log(buffer.subarray(9, 13));
+      fileHeaderFlags.pageCount = int32(buffer.subarray(9, 13));
+    }
+
+    return fileHeaderFlags;
+  };
+
   global.JBIG2 = {
     parse: function (buffer) {
-      return {
-        pageCount: 3
-      };
+      return decodeHeader(buffer);
     }
   };
 
