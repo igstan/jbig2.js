152b430f1b30adb7f6ef059468a21e69d0f8099f
Ionuț G. Stan <ionut.g.stan@gmail.com>
2012-05-15 00:55:28 +0300

    Move arithmetic coding tests at the top of the file
    It makes sense to keep these generic algorithms at the top of the file
    as they're segment agnostic and a prerequisite for following tests.

--- tests/tests.js
+++ tests/tests.js
@@ -70,6 +70,69 @@ var updateViewer = function (buffer) {
 };
 
 
+module("Arithmetic Coding");
+
+test("test decoding of sequence from Annex H, section 2 in the spec", function () {
+  var decoded = {
+    bytes: [],
+
+    bitIndex: 0,
+    byteIndex: 0,
+
+    pushBit: function (bit) {
+      var octet = this.bytes[this.byteIndex];
+      octet = octet === undefined ? 0x00 : octet;
+
+      octet = octet | (bit << (7 - this.bitIndex));
+      this.bytes[this.byteIndex] = octet;
+
+      this.bitIndex++;
+
+      if (this.bitIndex === 8) {
+        this.bitIndex = 0;
+        this.byteIndex++;
+      }
+    }
+  };
+
+  var sequence = [
+    0x00, 0x02, 0x00, 0x51, 0x00, 0x00, 0x00, 0xC0, 0x03, 0x52, 0x87, 0x2A,
+    0xAA, 0xAA, 0xAA, 0xAA, 0x82, 0xC0, 0x20, 0x00, 0xFC, 0xD7, 0x9E, 0xF6,
+    0xBF, 0x7F, 0xED, 0x90, 0x4F, 0x46, 0xA3, 0xBF
+  ];
+
+  var encodedData = JBIG2.streamFrom(new Uint8Array([
+    0x84, 0xC7, 0x3B, 0xFC, 0xE1, 0xA1, 0x43, 0x04, 0x02, 0x20, 0x00, 0x00,
+    0x41, 0x0D, 0xBB, 0x86, 0xF4, 0x31, 0x7F, 0xFF, 0x88, 0xFF, 0x37, 0x47,
+    0x1A, 0xDB, 0x6A, 0xDF, 0xFF, 0xAC
+  ]));
+
+  var CX = new ArithmeticContext(512, 0);
+  var decode = ArithmeticCoder.decoder(encodedData);
+  for (var i=0, max=256; i<max; i++) {
+    decoded.pushBit(decode(CX));
+  }
+
+  deepEqual(decoded.bytes, sequence);
+});
+
+var returnsSequentially = function (values) {
+  values = arguments.length === 1 ? values : Array.apply([], arguments);
+  return function () {
+    return values.shift();
+  };
+};
+
+// See the IADW decoding example on page 115 of the specification.
+test("arithmetic integer decoding", function () {
+  var context = new ArithmeticContext(512, 0);
+  var stubDecoder = returnsSequentially(0, 1, 0, 1, 0, 0, 0);
+  var n = JBIG2.decodeInteger(context, stubDecoder);
+
+  equal(n, 12);
+});
+
+
 module("File Header");
 
 asyncTest("throws on invalid file header ID", function () {
@@ -331,69 +394,6 @@ test("seventeenth segment in Annex H", function () {
     [1,_,_,_,_,1],
     [_,1,1,1,1,1]
   ]);
-});
-
-
-module("Arithmetic Coding");
-
-test("test decoding of sequence from Annex H, section 2 in the spec", function () {
-  var decoded = {
-    bytes: [],
-
-    bitIndex: 0,
-    byteIndex: 0,
-
-    pushBit: function (bit) {
-      var octet = this.bytes[this.byteIndex];
-      octet = octet === undefined ? 0x00 : octet;
-
-      octet = octet | (bit << (7 - this.bitIndex));
-      this.bytes[this.byteIndex] = octet;
-
-      this.bitIndex++;
-
-      if (this.bitIndex === 8) {
-        this.bitIndex = 0;
-        this.byteIndex++;
-      }
-    }
-  };
-
-  var sequence = [
-    0x00, 0x02, 0x00, 0x51, 0x00, 0x00, 0x00, 0xC0, 0x03, 0x52, 0x87, 0x2A,
-    0xAA, 0xAA, 0xAA, 0xAA, 0x82, 0xC0, 0x20, 0x00, 0xFC, 0xD7, 0x9E, 0xF6,
-    0xBF, 0x7F, 0xED, 0x90, 0x4F, 0x46, 0xA3, 0xBF
-  ];
-
-  var encodedData = JBIG2.streamFrom(new Uint8Array([
-    0x84, 0xC7, 0x3B, 0xFC, 0xE1, 0xA1, 0x43, 0x04, 0x02, 0x20, 0x00, 0x00,
-    0x41, 0x0D, 0xBB, 0x86, 0xF4, 0x31, 0x7F, 0xFF, 0x88, 0xFF, 0x37, 0x47,
-    0x1A, 0xDB, 0x6A, 0xDF, 0xFF, 0xAC
-  ]));
-
-  var CX = new ArithmeticContext(512, 0);
-  var decode = ArithmeticCoder.decoder(encodedData);
-  for (var i=0, max=256; i<max; i++) {
-    decoded.pushBit(decode(CX));
-  }
-
-  deepEqual(decoded.bytes, sequence);
-});
-
-var returnsSequentially = function (values) {
-  values = arguments.length === 1 ? values : Array.apply([], arguments);
-  return function () {
-    return values.shift();
-  };
-};
-
-// See the IADW decoding example on page 115 of the specification.
-test("arithmetic integer decoding", function () {
-  var context = new ArithmeticContext(512, 0);
-  var stubDecoder = returnsSequentially(0, 1, 0, 1, 0, 0, 0);
-  var n = JBIG2.decodeInteger(context, stubDecoder);
-
-  equal(n, 12);
 });
 
 
